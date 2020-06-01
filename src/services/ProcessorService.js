947f3e7030458cc4841a72d970865a8f2616686e
CWD <cwd@cloudspokes.com>
2020-06-01 17:57:31 -0400

    derp.
    lowercased the var name

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -180,7 +180,7 @@ createGroup.schema = {
           createdBy: joi.string(),
           createdAt: joi.date(),
           ssoId: joi.string().max(100).allow('', null),
-          organizationId: Joi.optionalId(),
+          organizationId: joi.optionalId(),
           status: joi.string()
         })
         .required()
@@ -307,7 +307,7 @@ updateGroup.schema = {
           createdBy: joi.string(),
           createdAt: joi.date(),
           ssoId: joi.string().max(100).allow('', null),
-          organizationId: Joi.optionalId(),
+          organizationId: joi.optionalId(),
           status: joi.string()
         })
         .required()
