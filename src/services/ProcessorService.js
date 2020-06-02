25aa36d8aa3640ca4047c8638493f543b9d4a06e
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2020-06-02 11:30:01 +0530

    made organizationId optional

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -180,7 +180,7 @@ createGroup.schema = {
           createdBy: joi.string(),
           createdAt: joi.date(),
           ssoId: joi.string().max(100).allow('', null),
-          organizationId: joi.string(),
+          organizationId: joi.string().allow('', null),
           status: joi.string()
         })
         .required()
@@ -307,7 +307,7 @@ updateGroup.schema = {
           createdBy: joi.string(),
           createdAt: joi.date(),
           ssoId: joi.string().max(100).allow('', null),
-          organizationId: joi.string(),
+          organizationId: joi.string().allow('', null),
           status: joi.string()
         })
         .required()
