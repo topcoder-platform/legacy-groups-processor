c459389cb16b54f17bc4fb3664a530378b77d061
LieutenantRoger <gair_2015@hotmail.com>
2022-04-13 21:51:04 +0800

    reset some code

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -76,8 +76,8 @@ async function createGroup (message) {
     const rawPayload = {
       name: _.get(message, 'payload.name'),
       description: _.get(message, 'payload.description'),
-      private_group: !!_.get(message, 'payload.privateGroup'),
-      self_register: !!_.get(message, 'payload.selfRegister'),
+      private_group: _.get(message, 'payload.privateGroup') ? true : false,
+      self_register: _.get(message, 'payload.selfRegister') ? true : false,
       ...(_.get(message, 'payload.createdBy') ? {createdBy: Number(_.get(message, 'payload.createdBy'))} : {}),
       ...(_.get(message, 'payload.createdBy') ? {modifiedBy: Number(_.get(message, 'payload.createdBy'))} : {}),
       createdAt: timestamp,
@@ -187,8 +187,8 @@ async function updateGroup (message) {
     const updateParams = [
       _.get(message, 'payload.name'),
       _.get(message, 'payload.description'),
-      !!_.get(message, 'payload.privateGroup'),
-      !!_.get(message, 'payload.selfRegister'),
+      _.get(message, 'payload.privateGroup') ? true : false,
+      _.get(message, 'payload.selfRegister') ? true : false,
       ...(_.get(message, 'payload.updatedBy') ? [Number(_.get(message, 'payload.updatedBy'))] : []),
       timestamp,
       Number(_.get(message, 'payload.oldId'))
